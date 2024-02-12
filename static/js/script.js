5733b965bad6d5fb74fd23d330792eb8e3b94002
Saurabh Kumar Jha <108005034+054saurabh@users.noreply.github.com>
2024-02-13 03:56:21 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+const squ = document.getElementById("sq");
+const bhk = document.getElementById("BHK");
+const bcony = document.getElementById("balcony");
+const sqrt = document.querySelector('.sqrt');
+
+console.log(squ.value);
